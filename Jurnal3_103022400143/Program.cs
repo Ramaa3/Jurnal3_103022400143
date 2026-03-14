4705f040d8ba0a3cd76a302148a4ef127c776d3c
Ramaa3 <ramnasyaifaarliansyah@student.telkomuniversity.ac.id>
2026-03-14 08:32:39 +0700

    first commit

--- Jurnal3_103022400143/Program.cs
+++ Jurnal3_103022400143/Program.cs
@@ -2,7 +2,6 @@ namespace Jurnal3_103022400143
 {
     internal static class Program
     {
-        /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
